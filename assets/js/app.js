05385821ccf9f181aa801cf9ce31a29f9a7ff21e
Age <ajfleck13@gmail.com>
2018-10-03 03:59:14 -0500

    More stuff

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,34 +1,105 @@
-const buttonholder = $("#buttonholder");
+const buttonholder = $("#recentbuttons");
 const baseURL = "https://min-api.cryptocompare.com";
-
-const cryptoList = ["BTC", "ETH"];
+const cryptoList = ["BTC", "ETH", "BCH", "XRP"];
+let compareCurrency = "USD";
+let cryptoType = "BTC";
+let currentInfo;
 
 const requestInformation = function()
 {
-    let currency = $(this).val();
-    ajaxFullPriceData(currency);
+    cryptoType = $(this).val();
+    ajaxFullPriceData();
 }
 
 const ajaxFullPriceData = function()
 {
-    let queryurl = baseURL + '/data/pricemultifull?' + $.param({
-        'fsym': currency,
+    let pricequeryurl = baseURL + '/data/pricemultifull?' + $.param({
+        'fsyms': cryptoType,
         'tsyms': "BTC,USD,EUR",
     });
 
-    console.log(queryurl);
-
     $.ajax({
-        url: queryurl,
+        url: pricequeryurl,
         method: "GET"
     }).then(function(response){
         console.log(response);
+        currentInfo = response;
     })
+
+    let newsqueryurl = baseURL + '/data/v2/news/?' + $.param({
+        'categories': cryptoType,
+        'lang': 'EN'
+    })
+
+    $.ajax({
+        url: newsqueryurl,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+        displayNews(response);
+    })
+
+    //displayInformation();
+}
+
+const displayInformation = function(){
+    let results = $("#resultswindow");
+    results.empty();
+    results.append(`<img src="https://www.cryptocompare.com${nameList[cryptoType].imageurl}" style="float: right; width:15vw; height:15vw;" alt="logo">`)
+    results.append(`<h1>${nameList[cryptoType].fullname}</h1>`)
+    let table = $("<table>");
+
+    let header = $("<thead>");
+    header.append(`<th>Information</th><th>Value</th>`)
+    table.append(header);
+
+    let body = $("<tbody>");
+    let information = currentInfo.DISPLAY[cryptoType][compareCurrency];
+    console.log(information);
+    let keys = Object.keys(information);
+    for(let i = 0; i < keys.length; i++)
+    {
+        let row = $("<tr>");
+        row.append(`<td>${keys[i]}<td>`)
+        row.append(`<td>${information[keys[i]]}`);
+        body.append(row);
+    }
+    table.append(body);
+
+    results.append(table);
+}
+
+const displayNews = function(news) {
+    let newsarray = news.Data;
+    let newsarea = $("#currencyNews");
+
+    for(let i = 0; i < newsarray.length && i < 10; i++)
+    {
+        let card = createMediaCard(newsarray[i]);
+        newsarea.append(card);
+    }
+}
+
+const createMediaCard = function(newsobject) {
+    let card = $(`<div class="media bg-success">`);
+    card.append(`<img class="align-self-center mr-3 mediacardimage" src="${newsobject.imageurl}" alt="Generic placeholder image">`);
+    let mediabody = $(`<div class="media-body">`);
+    mediabody.append(`<h5 class="mt-0">${newsobject.title}</h5>`);
+    mediabody.append(`<p>${newsobject.body}</p>`);
+    card.append(mediabody);
+    return card;
 }
 
 const appendButton = function(cryptoSymbol)
 {
-    let button = $(`<button class="bg-primary stockbutton" value="${cryptoSymbol}">`);
+    //Check if button from this crypto type already exists
+    if(document.getElementById(cryptoSymbol) !== null)
+    {
+        //If so do not append
+        return;
+    }
+
+    let button = $(`<button class="bg-primary stockbutton" value="${cryptoSymbol}" id="${cryptoSymbol}">`);
     button.text(cryptoSymbol);
     button.click(requestInformation);
     buttonholder.append("<br>");
@@ -42,6 +113,7 @@ for(let i = 0; i < cryptoList.length; i++)
 
 
 let validationList = [];
+let nameList = {};
 const retrieveValidationList = function()
 {
     $.ajax({
@@ -51,8 +123,46 @@ const retrieveValidationList = function()
         let datakeys = Object.keys(response.Data);
         for(let i = 0; i < datakeys.length; i++)
         {
-            validationList.push(response.Data[datakeys[i]].Symbol);
+            let symbol = response.Data[datakeys[i]].Symbol
+            validationList.push(symbol);
+            nameList[symbol] = {
+                imageurl: response.Data[datakeys[i]].ImageUrl,
+                fullname: response.Data[datakeys[i]].FullName
+            }
         }
     })
 }
 retrieveValidationList();
+
+const changeCurrencyType = function() {
+    compareCurrency = $(this).val();
+    console.log(compareCurrency);
+    displayInformation();
+}
+
+$(".currencytype").click(changeCurrencyType);
+
+const doSearch = function() {
+    let searchinput = $("#searchInput");
+    let inputvalue = searchinput.val().toUpperCase();
+
+    if(!validationList.includes(inputvalue))
+    {
+        return;
+    }
+
+    searchinput.val("");
+    appendButton(inputvalue);
+
+    cryptoType = inputvalue;
+    ajaxFullPriceData();
+}
+
+$("#Search").click(doSearch);
+
+const clearSearch = function() {
+    let searchinput = $("#searchInput");
+    searchinput.val("");
+}
+
+$("#Clear").click(clearSearch);
